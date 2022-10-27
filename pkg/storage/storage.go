ba2e78e5bab886d83fce35fd00abeb6530920428
Shivansh Vij <shivanshvij@outlook.com>
2022-10-27 14:54:32 -0700

    Updating storage to have less strict requirements

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -14,8 +14,6 @@ type Storage interface {
 	UserExists(id string) (bool, error)
 
 	GetAPIKey(id string) (*token.APIKey, error)
-	CreateAPIKey(apiKey *token.APIKey) error
 
 	GetServiceKey(id string, valid ServiceKeyValid, update ServiceKeyUpdate) (*token.ServiceKey, error)
-	CreateServiceKey(serviceKey *token.ServiceKey) error
 }
