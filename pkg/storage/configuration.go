b26a9e5fc9e34e59319f1f8d4da5c46753fd7ea8
Shivansh Vij <shivanshvij@outlook.com>
2025-07-30 17:53:08 -0700

    feat: adding configuration interface
    Signed-off-by: Shivansh Vij <shivanshvij@outlook.com>

---
+++ pkg/storage/configuration.go
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier: Apache-2.0
+
+package storage
+
+import "context"
+
+type Configuration struct {
+}
+
+// ConfigurationReadProvider is the read-only storage interface for Configurations
+type ConfigurationReadProvider interface {
+	// GetConfiguration returns the Configuration
+	GetConfiguration(ctx context.Context) (Configuration, error)
+}
+
+// ConfigurationWriteProvider is the write-only storage interface for Configurations
+type ConfigurationWriteProvider interface {
+	// SetConfiguration sets the Configuration
+	SetConfiguration(ctx context.Context, configuration Configuration) error
+}
+
+// ConfigurationProvider is the storage interface for Configurations
+type ConfigurationProvider interface {
+	// ConfigurationReadProvider is the read-only storage interfaces for Configurations
+	ConfigurationReadProvider
+
+	// ConfigurationWriteProvider is the write-only storage interfaces for Configurations
+	ConfigurationWriteProvider
+}
