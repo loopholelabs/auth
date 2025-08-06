0331a9d88beb4acf3d0e636318cc1a532fc59c4d
Shivansh Vij <shivanshvij@outlook.com>
2025-08-05 18:23:56 -0700

    refactor: moving generated code into its own package

--- internal/db/generated/organizations.sql.go
+++ internal/db/generated/organizations.sql.go
@@ -3,7 +3,7 @@
 //   sqlc v1.29.0
 // source: organizations.sql
 
-package db
+package generated
 
 import (
 	"context"
--- internal/db/generated/users.sql.go
+++ internal/db/generated/users.sql.go
@@ -3,7 +3,7 @@
 //   sqlc v1.29.0
 // source: users.sql
 
-package db
+package generated
 
 import (
 	"context"
