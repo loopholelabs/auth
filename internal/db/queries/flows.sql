935a7a28e5a7289d2ae3192c07e93e5692940369
Shivansh Vij <shivanshvij@outlook.com>
2025-08-05 20:10:14 -0700

    feat: initial github flow
    Signed-off-by: Shivansh Vij <shivanshvij@outlook.com>

---
+++ internal/db/queries/flows.sql
@@ -0,0 +1,15 @@
+-- name: CreateGithubOAuthFlow :exec
+INSERT INTO github_oauth_flows (identifier, device_identifier, user_identifier, verifier, challenge, next_url,
+                                created_at)
+VALUES (sqlc.arg(identifier), sqlc.arg(device_identifier), sqlc.arg(user_identifier), sqlc.arg(verifier),
+        sqlc.arg(challenge), sqlc.arg(next_url), CURRENT_TIMESTAMP);
+
+-- name: GetGithubOAuthFlowByIdentifier :one
+SELECT *
+FROM github_oauth_flows
+WHERE identifier = sqlc.arg(identifier) LIMIT 1;
+
+-- name: DeleteGithubOAuthFlowByIdentifier :exec
+DELETE
+FROM github_oauth_flows
+WHERE identifier = ?;
--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -1,4 +1,24 @@
+//SPDX-License-Identifier: Apache-2.0
+
 package controller
 
+import (
+	"github.com/loopholelabs/logging/types"
+
+	"github.com/loopholelabs/auth/internal/db"
+	"github.com/loopholelabs/auth/pkg/controller/flows/github"
+)
+
 type Controller struct {
+	logger types.Logger
+	db     *db.DB
+
+	github *github.Github
+}
+
+func New(db *db.DB, logger types.Logger) *Controller {
+	return &Controller{
+		logger: logger.SubLogger("CONTROLLER"),
+		db:     db,
+	}
 }
