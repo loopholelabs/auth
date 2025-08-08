fdca6bd80e34a53284d41689bf9b2096757bbc3d
Shivansh Vij <shivanshvij@outlook.com>
2025-08-07 20:46:14 -0700

    feat: adding additional membership and session refresh-specific functions
    Signed-off-by: Shivansh Vij <shivanshvij@outlook.com>

--- internal/db/queries/memberships.sql
+++ internal/db/queries/memberships.sql
@@ -1,5 +1,15 @@
+-- name: CreateMembership :exec
+INSERT INTO memberships (user_identifier, organization_identifier, role, created_at)
+VALUES (sqlc.arg(user_identifier), sqlc.arg(organization_identifier), LOWER(sqlc.arg(role)), CURRENT_TIMESTAMP);
+
 -- name: GetMembershipByUserIdentifierAndOrganizationIdentifier :one
 SELECT *
 FROM memberships
 WHERE user_identifier = sqlc.arg(user_identifier)
   AND organization_identifier = sqlc.arg(organization_identifier) LIMIT 1;
+
+-- name: UpdateMembershipRoleByUserIdentifier :exec
+UPDATE memberships
+SET role = LOWER(sqlc.arg(role))
+WHERE user_identifier = sqlc.arg(user_identifier)
+  AND organization_identifier = sqlc.arg(organization_identifier);
