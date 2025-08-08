4abf23b39788b409a92c92aec7a87d362b8def69
Shivansh Vij <shivanshvij@outlook.com>
2025-08-07 20:58:16 -0700

    fix: using Roles as strings directly to allow for backward conversion
    Signed-off-by: Shivansh Vij <shivanshvij@outlook.com>

--- pkg/manager/role/role.go
+++ pkg/manager/role/role.go
@@ -2,26 +2,15 @@
 
 package role
 
-type Role uint32
+type Role string
 
 const (
-	OwnerRole Role = iota
-	AdminRole
-	MemberRole
-	ViewerRole
+	OwnerRole  Role = "owner"
+	AdminRole  Role = "admin"
+	MemberRole Role = "member"
+	ViewerRole Role = "viewer"
 )
 
 func (r Role) String() string {
-	switch r {
-	case OwnerRole:
-		return "owner"
-	case AdminRole:
-		return "admin"
-	case MemberRole:
-		return "member"
-	case ViewerRole:
-		return "viewer"
-	default:
-		return "unknown"
-	}
+	return string(r)
 }
