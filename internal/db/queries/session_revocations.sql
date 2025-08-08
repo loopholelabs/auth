6cc8cc5195cf4fd57ef3c70a2b4e24d3dd4075a5
Shivansh Vij <shivanshvij@outlook.com>
2025-08-08 15:31:46 -0700

    feat: tests for the session revocation implementation
    Signed-off-by: Shivansh Vij <shivanshvij@outlook.com>

--- internal/db/queries/session_revocations.sql
+++ internal/db/queries/session_revocations.sql
@@ -2,11 +2,11 @@
 INSERT INTO session_revocations (session_identifier, expires_at, created_at)
 VALUES (sqlc.arg(session_identifier), sqlc.arg(expires_at), CURRENT_TIMESTAMP);
 
+-- name: GetAllSessionRevocations :many
+SELECT *
+FROM session_revocations;
+
 -- name: DeleteExpiredSessionRevocations :execrows
 DELETE
 FROM session_revocations
 WHERE expires_at <= NOW();
-
--- name: GetAllSessionRevocations :many
-SELECT *
-FROM session_revocations;
