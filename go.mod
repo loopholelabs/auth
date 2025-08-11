4aac9da29159cf16f923be8bdff9d81ff75f2272
Shivansh Vij <shivanshvij@outlook.com>
2025-08-11 10:14:35 -0700

    feat: using v2 swagger
    Signed-off-by: Shivansh Vij <shivanshvij@outlook.com>

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.24.1
 tool (
 	github.com/pressly/goose/v3/cmd/goose
 	github.com/sqlc-dev/sqlc/cmd/sqlc
-	github.com/swaggo/swag/cmd/swag
+	github.com/swaggo/swag/v2/cmd/swag
 )
 
 require (
@@ -92,6 +92,7 @@ require (
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/jonboulle/clockwork v0.5.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/lufia/plan9stats v0.0.0-20250317134145-8bc96cf8fc35 // indirect
@@ -112,6 +113,8 @@ require (
 	github.com/moby/sys/user v0.4.0 // indirect
 	github.com/moby/sys/userns v0.1.0 // indirect
 	github.com/moby/term v0.5.2 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/ncruces/go-strftime v0.1.9 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
@@ -145,6 +148,8 @@ require (
 	github.com/sqlc-dev/sqlc v1.29.0 // indirect
 	github.com/stoewer/go-strcase v1.3.1 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
+	github.com/sv-tools/openapi v0.2.1 // indirect
+	github.com/swaggo/swag/v2 v2.0.0-rc4 // indirect
 	github.com/tetratelabs/wazero v1.9.0 // indirect
 	github.com/tklauser/go-sysconf v0.3.15 // indirect
 	github.com/tklauser/numcpus v0.10.0 // indirect
@@ -185,6 +190,7 @@ require (
 	google.golang.org/grpc v1.74.2 // indirect
 	google.golang.org/protobuf v1.36.7 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	howett.net/plist v1.0.1 // indirect
 	modernc.org/libc v1.66.6 // indirect
---
+++ pkg/api/v1/user/user.go
@@ -0,0 +1,84 @@
+//SPDX-License-Identifier: Apache-2.0
+
+package user
+
+import (
+	"github.com/gofiber/fiber/v2"
+
+	"github.com/loopholelabs/logging/types"
+
+	"github.com/loopholelabs/auth/internal/utils"
+	"github.com/loopholelabs/auth/pkg/api/options"
+)
+
+type User struct {
+	logger types.Logger
+	app    *fiber.App
+
+	options options.Options
+}
+
+func New(options options.Options, logger types.Logger) *User {
+	a := &User{
+		logger:  logger.SubLogger("USER"),
+		app:     utils.DefaultFiberApp(),
+		options: options,
+	}
+
+	a.app.Get("/info", a.info)
+
+	return a
+}
+
+func (a *User) App() *fiber.App {
+	return a.app
+}
+
+// info godoc
+// @Summary      info returns the information about a logged-in User
+// @Description  info returns the information about a logged-in User
+// @Tags         user, info
+// @Accept       json
+// @Produce      json
+// @Success      307
+// @Header       307 {string} Location "Redirects to User"
+// @Failure      400 {string} string
+// @Failure      401 {string} string
+// @Failure      404 {string} string
+// @Failure      500 {string} string
+// @Router       /flows/github/login [get]
+func (a *User) info(ctx *fiber.Ctx) error {
+	a.logger.Debug().Str("IP", ctx.IP()).Msg("info")
+	if a.options.Manager.User() == nil {
+		return ctx.Status(fiber.StatusUnauthorized).SendString("github provider is not enabled")
+	}
+
+	nextURL := ctx.Query("next")
+	if nextURL == "" {
+		return ctx.Status(fiber.StatusBadRequest).SendString("next URL is required")
+	}
+
+	var err error
+	var deviceIdentifier string
+	code := ctx.Query("code")
+	if code != "" && len(code) == 8 {
+		if a.options.Manager.Device() == nil {
+			return ctx.Status(fiber.StatusUnauthorized).SendString("device provider is not enabled")
+		}
+		deviceIdentifier, err = a.options.Manager.Device().ExistsFlow(ctx.Context(), code)
+		if err != nil {
+			a.logger.Error().Err(err).Msg("error checking if flow exists")
+			return ctx.SendStatus(fiber.StatusInternalServerError)
+		}
+		if deviceIdentifier == "" {
+			return ctx.Status(fiber.StatusNotFound).SendString("device flow does not exist")
+		}
+	}
+
+	redirect, err := a.options.Manager.User().CreateFlow(ctx.Context(), deviceIdentifier, "", nextURL)
+	if err != nil {
+		a.logger.Error().Err(err).Msg("failed to get redirect")
+		return ctx.SendStatus(fiber.StatusInternalServerError)
+	}
+	return ctx.Redirect(redirect, fiber.StatusTemporaryRedirect)
+}
