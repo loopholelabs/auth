deda59e001acb93c631afb98a8ce67a1dca5ddb3
Shivansh Vij <shivanshvij@outlook.com>
2025-08-13 17:10:24 -0700

    fix: flag parsing from environment variables
    Signed-off-by: Shivansh Vij <shivanshvij@outlook.com>

--- internal/config/api.go
+++ internal/config/api.go
@@ -65,10 +65,26 @@ func NewAPI() *API {
 }
 
 func (c *API) RequiredFlags(cmd *cobra.Command) error {
-	cmd.Flags().StringVar(&c.ListenAddress, "listen_address", DefaultListenAddress, "the address to listen on")
-	cmd.Flags().BoolVar(&c.TLS, "tls", false, "whether or not to use TLS")
-	cmd.Flags().StringVar(&c.Endpoint, "endpoint", DefaultEndpoint, "the api endpoint")
-	cmd.Flags().StringVar(&c.Issuer, "issuer", "authentication-service", "the issuer")
+	cmd.Flags().StringVar(&c.ListenAddress, "api-listen-address", DefaultListenAddress, "the address to listen on")
+	cmd.Flags().BoolVar(&c.TLS, "api-tls", false, "whether or not to use TLS")
+	cmd.Flags().StringVar(&c.Endpoint, "api-endpoint", DefaultEndpoint, "the api endpoint")
+	cmd.Flags().StringVar(&c.Issuer, "api-issuer", "authentication-service", "the issuer")
+	cmd.Flags().BoolVar(&c.Github.Enabled, "api-github-enabled", false, "enable github provider")
+	cmd.Flags().StringVar(&c.Github.ClientID, "api-github-client-id", "", "github provider's client ID")
+	cmd.Flags().StringVar(&c.Github.ClientSecret, "api-github-client-secret", "", "github provider's client secret")
+	cmd.Flags().BoolVar(&c.Google.Enabled, "api-google-enabled", false, "enable google provider")
+	cmd.Flags().StringVar(&c.Google.ClientID, "api-google-client-id", "", "google provider's client ID")
+	cmd.Flags().StringVar(&c.Google.ClientSecret, "api-google-client-secret", "", "google provider's client secret")
+	cmd.Flags().BoolVar(&c.Magic.Enabled, "api-magic-enabled", false, "enable magic provider")
+	cmd.Flags().BoolVar(&c.Device.Enabled, "api-device-enabled", false, "enable device provider")
+	cmd.Flags().BoolVar(&c.Mailer.Enabled, "api-mailer-enabled", false, "enable mailer provider")
+	cmd.Flags().StringVar(&c.Mailer.SMTPHost, "api-mailer-smtp-host", "", "mailer smtp host")
+	cmd.Flags().IntVar(&c.Mailer.SMTPPort, "api-mailer-smtp-port", 0, "mailer provider's smtp port")
+	cmd.Flags().StringVar(&c.Mailer.SMTPUsername, "api-mailer-smtp-username", "", "mailer provider's smtp username")
+	cmd.Flags().StringVar(&c.Mailer.SMTPPassword, "api-mailer-smtp-password", "", "mailer provider's smtp password")
+	cmd.Flags().StringVar(&c.Mailer.FromEmail, "api-mailer-from-email", "", "mailer provider's from email")
+	cmd.Flags().StringVar(&c.Mailer.FromName, "api-mailer-from-name", "", "mailer provider's from name")
+	cmd.Flags().StringVar(&c.Mailer.MagicLinkTemplatePath, "api-mailer-magic-link-template", "", "mailer provider's magic link template path")
 
 	return nil
 }
